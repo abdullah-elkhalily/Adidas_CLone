2ad9aae9b2c969696b0e28b43aa93e4a44510d90
abdullah-elkhalily <abdullahhussiin1125@gmail.com>
2023-12-13 16:32:14 +0200

    adidas

--- src/Components/EmptyCart/EmptyCart.jsx
+++ src/Components/EmptyCart/EmptyCart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Empty.css"
 import { useNavigate } from 'react-router-dom';
-export const EmptyCart = () => {
+export const clearEmptyCart = () => {
 
   const Navigate =useNavigate()
   return (
